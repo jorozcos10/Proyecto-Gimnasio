cda1a4a05dc685f832a4f54503178b669fef2c2b
jorozcos10 <armando.149@hotmail.com>
2024-11-24 13:45:04 -0600

    subiendo nuevas clases y forms

---
+++ OLIMPO/Controlador/ControladorClase.cs
@@ -0,0 +1,50 @@
+﻿using OLIMPO.Helpers;
+using OLIMPO.Modelo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLIMPO.Controlador
+{
+    public class ControladorClase
+    {
+        private string archivo = FileHelper.GetFilePath("clases.csv");
+        public List<ModeloClase> LeerDatos()
+        {
+            List<ModeloClase> lista = new List<ModeloClase>();
+
+            // Verifica si el archivo existe
+            if (File.Exists(archivo))
+            {
+                using (StreamReader sr = new StreamReader(archivo)) // Lee el archivo línea por línea
+                {
+                    // Omitir la cabecera
+                    sr.ReadLine(); // Lee y descarta la primera línea
+
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)  // Lee cada línea del archivo
+                    {
+                        // Separa la línea en partes usando la coma como delimitador
+                        string[] partes = linea.Split(',');
+
+                        // Crea un objeto ModeloClase a partir de las partes
+                        ModeloClase clase = new ModeloClase
+                        {
+                            Id = int.Parse(partes[0]),
+                            Nombre = partes[1],
+                        };
+
+                        // Agrega el objeto a la lista
+                        lista.Add(clase);
+                    }
+                }
+            }
+
+            return lista;  // Devuelve la lista de clases
+        }
+    }
+
+}
